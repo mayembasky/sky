9bcc5faee12b2731d3fa3ca18446ac728a96e2e9
unknown <kerenskyhappuc@gmail.com>
2023-08-28 17:02:16 +0200

    first commit

---
+++ Assets/marchepersonnage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class marchepersonnage : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public float speed=5f;
+    Rigidbody2D rb;
+    void Start()
+    {
+      rb =GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal=Input.GetAxis("Horizontal");
+        float moveVertical=Input.GetAxis("Vertical");
+        Vector2 move=new Vector2(moveHorizontal,moveVertical);
+        rb.velocity=move*speed ;
+    }
+}
