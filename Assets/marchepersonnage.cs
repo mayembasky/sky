6f5e2c8a7c7a9d9cb9f307b414e404982c44f203
unknown <kerenskyhappuc@gmail.com>
2023-08-30 15:55:20 +0200

    gestion de l animation et de la camera

--- Assets/marchepersonnage.cs
+++ Assets/marchepersonnage.cs
@@ -8,17 +8,41 @@ public class marchepersonnage : MonoBehaviour
     // Start is called before the first frame update
     public float speed=5f;
     Rigidbody2D rb;
+    Animator anim;
     void Start()
     {
       rb =GetComponent<Rigidbody2D>();
+      anim=GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
+
     {
+        
         float moveHorizontal=Input.GetAxis("Horizontal");
         float moveVertical=Input.GetAxis("Vertical");
         Vector2 move=new Vector2(moveHorizontal,moveVertical);
-        rb.velocity=move*speed ;
+        rb.velocity=move*speed*Time.fixedDeltaTime; 
+        makeAnimation(moveHorizontal,moveVertical);
     }
+     void makeAnimation(float dirx,float diry){
+      if(dirx==0 && diry==0){
+        anim.SetInteger("dir",0);
+      }
+      else if(dirx>0)
+      {
+      anim.SetInteger("dir",2);
+      }
+      else if(dirx<0)
+      {
+        anim.SetInteger("dir",1);
+      }
+      else if(diry>0)
+      {
+      anim.SetInteger("dir",3);
+      }
+      else if (diry<0)
+      {
+        anim.SetInteger("dir",4);
+      }
+     }
 }
---
+++ Assets/script/marchepersonnage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marchepersonnage : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public float speed=5f;
+    Rigidbody2D rb;
+   Animator anim;
+    void Start()
+    {
+      rb =GetComponent<Rigidbody2D>();
+      anim=GetComponent<Animator>();
+    }
+    void Update()
+
+    {
+        
+        float moveHorizontal=Input.GetAxis("Horizontal");
+        float moveVertical=Input.GetAxis("Vertical");
+        Vector2 move=new Vector2(moveHorizontal,moveVertical);
+        rb.velocity=move*speed*Time.fixedDeltaTime; 
+        makeAnimation(moveHorizontal,moveVertical);
+    }
+     void makeAnimation(float dirx,float diry){
+      if(dirx==0 && diry==0){
+        anim.SetInteger("dir",0);
+      }
+      else if(dirx>0)// right
+      {
+      anim.SetInteger("dir",2);
+      }
+      else if(dirx<0)// left
+      {
+        anim.SetInteger("dir",1);
+      }
+      else if(diry>0)// up
+      {
+      anim.SetInteger("dir",3);
+      }
+      else if (diry<0)// down
+      {
+        anim.SetInteger("dir",4);
+      }
+     }
+}
